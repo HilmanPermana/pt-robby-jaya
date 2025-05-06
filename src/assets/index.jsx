66482291211bb9b9b3defb195a04a9dee16c7ecc
HilmanPermana <96397184+HilmanPermana@users.noreply.github.com>
2025-05-06 20:01:04 +0700

    Create index.jsx

---
+++ src/assets/index.jsx
@@ -0,0 +1,17 @@
+export {default as tim_kami } from "./tim_kami.png";
+export {default as master_produk} from "./master_produk.png";
+export {default as cover_perusahaan} from "./cover_perusahaan.png";
+export {default as pabrik_1} from "./pabrik_1.png";
+export {default as packaging_1} from "./packaging_1.png";
+export {default as storage_2} from "./storage_2.1.png";
+export {default as produk_1} from "./p_cisalam_cup.png";
+export {default as produk_2} from "./p_galon.png";
+export {default as produk_3} from "./p_bywi_cup.png";
+export {default as produk_4} from "./p_cisalam_botol.png";
+export {default as logo_perusahaan} from "./logo_perusahaan.png"
+export {default as water_reservoir} from "./water_reservoir.png"
+export {default as water_filterisasi} from "./water_filterisasi.png"
+export {default as cup_filling} from "./cup_filling.png"
+export {default as bottle_filling} from "./bottle_filling.png"
+export {default as galon_filling} from "./galon_filling.png"
+export {default as packaging} from "./packaging.png"
