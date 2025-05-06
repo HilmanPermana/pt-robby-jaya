359bc2a35eea38f9c75560a63d18a506764751f9
HilmanPermana <96397184+HilmanPermana@users.noreply.github.com>
2025-05-06 19:55:57 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import About from "./components/About"
+import Services from "./components/Services"
+import Teams from "./components/Teams"
+import Project from "./components/Project"
+import Testimonial from "./components/Testimonial"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <> 
+    <Navbar/>
+    <Header/>
+    <About/>
+    <Services/>
+    <Project/>
+    <Teams/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
